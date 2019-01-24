38ad875c6858377f4533633c4b3c5221d8b482fb
Artur Antonov <arturom@yandex-team.ru>
2019-01-24 09:48:26 +0300

    Initial Commit

---
+++ Fingerspelling recognizer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Fingerspelling recognizer
+//
+//  Created by Artur Antonov on 24.01.2019.
+//  Copyright © 2019 aa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
